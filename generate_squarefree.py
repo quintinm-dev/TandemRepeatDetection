4d43e77b5becd2128628c2662d3b471cf27921ae
Quintin Muncaster <12143900+quintinm-dev@users.noreply.github.com>
2020-03-22 17:25:14 -0700

    add python script to generate long squarefree words

---
+++ generate_squarefree.py
@@ -0,0 +1,17 @@
+# Generates test input for 2 long square free words
+
+print'2'
+
+n=10**4 # word length
+
+# SOURCE: https://codegolf.stackexchange.com/a/10606
+# Generates squarefree word using difference of thue-morse sequence
+x=[0]
+while x==x[:n]:x+=[1-y for y in x]
+print''.join('ABC'[x[i+1]-x[i]]for i in range(n))
+
+# SOURCE: https://codegolf.stackexchange.com/a/10602
+# Generates square-free word using john leech's method
+s='A'
+while len(s)<=n:s=''.join('ABCBCACABBCAABCCABBCACABABCBCACABBCAABC'[ord(t)%5::3]for t in s)
+print s[:n]
