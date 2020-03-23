2e58c2aa2b59637b1d9747a649d9c198bad756e8
Quintin Muncaster <12143900+quintinm-dev@users.noreply.github.com>
2020-03-22 23:03:30 -0700

    fix CRLF line endings...

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 g++ -W -Wall -pedantic --std=c++17 -ggdb3 -o tr.o tr.cpp
+g++ -W -Wall -pedantic --std=c++17 -ggdb3 -o tr_fast.o tr_fast.cpp
 g++ -W -Wall -pedantic --std=c++17 -ggdb3 -o tr_bf.o tr_bf.cpp
 
 cd "tests/"
 FILES="$(ls -v in/*.in)"
-OUTPUT_DIR="out/"
+
+IMP_DIR="out/"
+# IMP_EXE="../tr.o"
+IMP_EXE="../tr_fast.o"
+
 STD_DIR="std/"
 STD_EXE="../tr_bf.o"
 
@@ -15,17 +20,17 @@ do
     echo "\nfile $file"
 
     # echo "running implementation"
-    ../tr.o < $f > ${OUTPUT_DIR}${file}.out
+    $IMP_EXE < $f > ${IMP_DIR}${file}.out
 
     # echo "running comparison standard"
     $STD_EXE < $f > ${STD_DIR}${file}.out
 
     # echo "diffing"
-    diff ${OUTPUT_DIR}${file}.out ${STD_DIR}${file}.out
+    diff ${IMP_DIR}${file}.out ${STD_DIR}${file}.out
 
     echo "no_count/total = \c"
-    no_count=$(grep -c "NO" ${OUTPUT_DIR}${file}.out)
-    total=$(wc -l < ${OUTPUT_DIR}${file}.out)
+    no_count=$(grep -c "NO" ${IMP_DIR}${file}.out)
+    total=$(wc -l < ${IMP_DIR}${file}.out)
     total=$((total+1)) # unix doesn't count last line as no CRLF
     ratio=$((100*no_count/total))
     echo "$no_count/$total = $ratio%"
